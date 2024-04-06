8cccc76a74c5eda90a9ecf1d48529ee6daed219a
Lara <75126217+little-evil-genius@users.noreply.github.com>
2024-04-06 23:56:22 +0200

    Create inplayquotes.lang.php

---
+++ inc/languages/deutsch_du/inplayquotes.lang.php
@@ -0,0 +1,56 @@
+<?php
+$l['inplayquotes_postbit'] = "Neues Inplayzitat";
+$l['inplayquotes_postbit_popup'] = "Neues Inplayzitat eintragen";
+$l['inplayquotes_postbit_popup_infos'] = "<b>Szene:</b> {1}<br><b>Zitat von:</b> {2}";
+$l['inplayquotes_postbit_popup_quote'] = "Folgendes Zitat als denkwürdigen Inplaymoment eintragen:";
+$l['inplayquotes_postbit_popup_button'] = "Zitat absenden";
+
+$l['inplayquotes_pm_add_quote_subject'] = "Du wurdest aus dem Inplay zitiert!";
+$l['inplayquotes_pm_add_quote_message'] = "Hallo {1},<br>ich habe folgendes Zitat aus der Szene \"<a href=\"showthread.php?tid={2}&pid={3}#pid{3}\">{4}</a>\" als denkwürdigen Inplaymoment eingetragen:<br><br>{5}";
+$l['inplayquotes_pm_add_reaction_subject'] = "Auf dein Inplayzitat wurde reagiert!";
+$l['inplayquotes_pm_add_reaction_message'] = "Hallo {1},<br>ich habe auf folgendes Zitat mit <img src=\"{2}\" style=\"16px\"> reagiert:<br><br>{3}";
+
+$l['inplayquotes_lists'] = "Listen";
+$l['inplayquotes_overview'] = "Alle Inplayzitat";
+$l['inplayquotes_overview_error_guest'] = "Du hast keine Berechtigungen die Inplayzitat einzusehen.";
+$l['inplayquotes_filter'] = "Filtern nach";
+$l['inplayquotes_filter_button'] = "Zitate durchsuchen";
+
+$l['inplayquotes_filter_headline_player'] = "nach Spieler:in";
+$l['inplayquotes_filter_select_player'] = "Spieler:in auswählen";
+$l['inplayquotes_filter_headline_character'] = "nach Charakter";
+$l['inplayquotes_filter_select_character'] = "Charackter auswählen";
+$l['inplayquotes_filter_select_character_formerly'] = " (ehem.)";
+$l['inplayquotes_filter_headline_timestamp'] = "nach Postdatum";
+$l['inplayquotes_filter_select_timestamp'] = "Zeitraum auswählen";
+
+$l['inplayquotes_index_headline'] = "Zufälliges Inplayzitat";
+$l['inplayquotes_index_link'] = "Alle Inplayzitate";
+$l['inplayquotes_index_bit_none'] = "es gibt noch keine denkwürdigen Inplaymoment!";
+
+$l['inplayquotes_memberprofile_headline'] = "Zitat aus dem Inplay";
+$l['inplayquotes_memberprofile_link'] = "Alle Zitate von {1}";
+$l['inplayquotes_memberprofile_bit_none'] = "Dieser Charakter wurde noch nicht aus dem Inplay zitiert!";
+
+$l['inplayquotes_allreactions_delete'] = "Alle Reaktionen löschen";
+$l['inplayquote_redirect_allreactions_delete'] = "Du hast erfolgreich alle Reaktion von diesem Inplayzitat gelöscht! Du  wirst nun zurück zur Übersicht der Inplayzitate geleitet";
+$l['inplayquotes_quote_delete'] = "Zitat löschen";
+$l['inplayquote_redirect_quote_delete'] = "Du hast erfolgreich ein das Inplayzitat gelöscht! Du  wirst nun zurück zur Übersicht der Inplayzitate geleitet";
+$l['inplayquote_redirect_reactions_delete'] = "Du hast erfolgreich die Reaktion von diesem Inplayzitat gelöscht! Du  wirst nun zurück zur Übersicht der Inplayzitate geleitet";
+
+
+$l['inplayquotes_reactions_add'] = "Reaktion hinzufügen";
+$l['inplayquotes_reactions_add_desc'] = "Smilie anklicken, um diesen als Reaktion hinzufügen";
+$l['inplayquotes_reactions_reacted'] = "Deine Reaktion";
+$l['inplayquotes_reactions_quote_preview'] = "<b>{1}</b>:<br>{2}";
+
+$l['inplayquote_redirect_add_inplayquote'] = "Du hast erfolgreich ein neues Inplayzitat hinzugefügt! Du  wirst nun zurück zum ursprünglichen Beitrag geleitet.";
+$l['inplayquote_redirect_add_reaction'] = "Du hast erfolgreich ein Reaktion hinzugefügt! Du  wirst nun zurück zur Übersicht der Inplayzitate geleitet.";
+$l['inplayquotes_add_inplayquote_error_allowgroups'] = "Du hast keine Berechtigungen ein Inplayzitat hinzuzufügen.";
+
+$l['inplayquotes_online_location'] = "Schaut sich gerade die <a href=\"misc.php?action=inplayquotes\">Übersicht der Inplayzitate</a> an.";
+
+$l['inplayquotes_alert_add_quote'] = '{1} hat dich aus der Szene <b>"{3}"</b> zitiert.';
+$l['myalerts_setting_inplayquotes_alert_add_quote'] = "Benachrichtigung erhalten wenn du aus dem Inplay zitiert wurdest?";
+$l['inplayquotes_alert_add_reaction'] = '{1} hat mit <img src="{3}" style="width:15px;" /> auf dein Inplayzitat reagiert';
+$l['myalerts_setting_inplayquotes_alert_add_reaction'] = "Benachrichtigung erhalten wenn jemand auf dein Inplayzitat reagiert hat?";
