a1948b21f85b63a9c60b1baca82116698652becf
Lara <75126217+little-evil-genius@users.noreply.github.com>
2024-04-06 23:57:12 +0200

    Create inplayquotes.lang.php

---
+++ inc/languages/deutsch_du/admin/inplayquotes.lang.php
@@ -0,0 +1,36 @@
+<?php
+$l['inplayquotes_error_accountswitcher'] = "Das Plugin <a href=\"http://doylecc.altervista.org/bb/downloads.php?dlid=26&cat=2\" target=\"_blank\">\"Enhanced Account Switcher\"</a> muss installiert sein!";
+
+$l['inplayquotes_permission'] = "Kann die Reaktionsoptionen der Inplayzitate verwalten?";
+$l['inplayquotes_manage'] = "Inplayzitate - Reaktionen";
+$l['inplayquotes_manage_header'] = "Reaktionsoptionen der Inplayzitate verwalten";
+
+$l['inplayquotes_overview'] = "Inplayzitate Reaktionen verwalten";
+$l['inplayquotes_overview_desc'] = "Hier kannst du Reaktionen für die Inplayziatet bearbeiten und löschen.";
+$l['inplayquotes_overview_image'] = "Bild";
+$l['inplayquotes_overview_name'] = "Name";
+$l['inplayquotes_overview_option'] = "Optionen";
+$l['inplayquotes_overview_option_edit'] = "Bearbeiten";
+$l['inplayquotes_overview_option_delete'] = "Löschen";
+$l['inplayquotes_overview_option_delete_notice'] = "Soll dieses Upload-Element wirklich gelöscht werden? Bisherige Reaktionen mit dieser Reaktion werden auch gelöscht.";
+$l['inplayquotes_overview_no_elements'] = "Bisher wurden keine Reaktionsmöglichkeiten für die Inplayzitate erstellt.";
+
+$l['inplayquotes_add'] = "Neue Reaktion hinzufügen";
+$l['inplayquotes_add_desc'] = "Hier kannst du eine Reaktionen für die Inplayziatet hinzufügen.";
+$l['inplayquotes_add_error_name'] = "Du hast keinen Namen für diese Reaktion angegeben.";
+$l['inplayquotes_add_error_image'] = "Du hast keinen Pfad angegeben";
+$l['inplayquotes_add_logadmin'] = "Ein neue Reaktion wurde hinzugefügt ";
+$l['inplayquotes_add_flash'] = "Die neue Reaktion für die Inplayzitate wurde erfolgreich erstellt.";
+$l['inplayquotes_add_name'] = "Name <em>*</em>";
+$l['inplayquotes_add_image'] = "Pfad zur Grafik <em>*</em>";
+$l['inplayquotes_add_image_desc'] = "Dies ist der Pfad zur Bilddatei der Reaktion.";
+$l['inplayquotes_add_button'] = "Reaktion speichern";
+
+$l['inplayquotes_edit'] = "Reaktion bearbeiten";
+$l['inplayquotes_edit_desc'] = "Hier kannst du eine Reaktion bearbeiten.";
+$l['inplayquotes_edit_logadmin'] = "Die Reaktion wurde bearbeitet ";
+$l['inplayquotes_edit_flash'] = "Die Reaktion für die Inplayzitate wurde erfolgreich bearbeitet.";
+
+$l['inplayquotes_delete_error_invalid'] = "Ungültige Option";
+$l['inplayquotes_delete_logadmin'] = "Upload-Elemente gelöscht ";
+$l['inplayquotes_delete_flash'] = "Die Reaktion {1} für die Inplayzitate wurde erfolgreich gelöscht.";
