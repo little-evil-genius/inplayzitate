298aaf85a51cd016d5d5243c3b82612f8dbacfb2
Lara <75126217+little-evil-genius@users.noreply.github.com>
2024-05-07 09:11:50 +0200

    Update update_inplayquotes.php

--- update_inplayquotes.php
+++ update_inplayquotes.php
@@ -1,8 +1,6 @@
 <?php
 define("IN_MYBB", 1);
 require("global.php");
-error_reporting(-1);
-ini_set('display_errors', 1);
 
 global $db, $mybb, $lang;
 echo (
@@ -28,41 +26,29 @@ if ($mybb->usergroup['canmodcp'] == 1 AND !$db->table_exists("inplayquotes_react
   echo "<h1>Update Script für die Übertragung der Inplayzitate</h1>";
   echo "<p>Ehemalige Inplayzitate von dem Plugin Inplayquotes 2.0 oder 3.0 von Sparksfly mit einem Klick übertragen</p>";
 
-  echo "<h1>1. Schritt Datenbanktabelle umbennen</h1>";
+  echo "<h1>Datenbanktabelle der Inplayzitate sichern</h1>";
   echo "<p>Jules Plugin darf nicht deinstalliert sein und meins noch nicht installiert!</p>";
   echo '<form action="" method="post">';
-  echo '<input type="submit" name="database_rename" value="Datenbanktabelle umbennen">';
+  echo '<input type="submit" name="database_copy" value="Datenbanktabelle sichern">';
   echo '</form>';
 
-  if (isset($_POST['database_rename'])) {
+  if (isset($_POST['database_copy'])) {
 
-    $old_table_name = TABLE_PREFIX . "inplayquotes";
-    $new_table_name = TABLE_PREFIX . "inplayquotes_jule";
-
-    $query = "RENAME TABLE $old_table_name TO $new_table_name";
-
-    if ($db->table_exists("ingamequotes_likes")) {
-
-        $old_table_name_katja = TABLE_PREFIX . "ingamequotes_likes";
-        $new_table_name_katja = TABLE_PREFIX . "ingamequotes_likes_jule";
-
-        $db->write_query($query = "RENAME TABLE $old_table_name_katja TO $new_table_name_katja");
-    }
+    $query = "CREATE TABLE IF NOT EXISTS inplayquotes_copy LIKE inplayquotes;
+              INSERT INTO inplayquotes_copy SELECT * FROM inplayquotes;";
 
     // Führe das Update aus
     if ($db->write_query($query)) {
-        echo "Die Tabelle wurde erfolgreich umbenannt. Jules Plugin kann deinstalliert werden und dann meins installiert werden.";
+        echo "Die Tabelle wurde erfolgreich gesichert. Jules Plugin kann deinstalliert werden und dann meins installiert werden.";
     } else {
-        echo "Fehler beim Umbenennen der Tabelle.";
+        echo "Fehler beim sichern der Tabelle.";
     }
-
-
   }
 
   echo '<div style="width:100%; background-color: rgb(121 123 123 / 50%); display: flex; position:fixed; bottom:0;right:0; height:50px; justify-content: center; align-items:center; gap:20px;">
 <div> <a href="https://github.com/little-evil-genius/inplayzitate" target="_blank">Github Rep</a></div>
 <div> <b>Kontakt:</b> little.evil.genius (Discord)</div>
-<div> <b>Support:</b>  <a href="LINK">SG Thread</a> oder per Discord</div>
+<div> <b>Support:</b>  <a href="https://storming-gates.de/showthread.php?tid=1039057">SG Thread</a> oder per Discord</div>
 </div>';
 } else {
   echo "<h1>Kein Zugriff</h1>";
