cea44dbc0f96095fd1880954d6a8292e991fa686
Lara <75126217+little-evil-genius@users.noreply.github.com>
2025-05-19 08:58:58 +0200

    Update inplayquotes.php

--- inc/plugins/inplayquotes.php
+++ inc/plugins/inplayquotes.php
@@ -784,6 +784,8 @@ function inplayquotes_admin_update_plugin(&$table) {
 
             // Ob im Master Style die Überprüfung vorhanden ist
             $masterstylesheet = $db->fetch_field($db->query("SELECT stylesheet FROM ".TABLE_PREFIX."themestylesheets WHERE tid = 1 AND name = 'inplayquotes.css'"), "stylesheet");
+            $masterstylesheet = (string)($masterstylesheet ?? '');
+            $update_string = (string)($update_string ?? '');
             $pos = strpos($masterstylesheet, $update_string);
             if ($pos === false) { // nicht vorhanden 
             
